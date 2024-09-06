991a1f187924a099f48748eb403773be348123e1
Juan Esteban Ramirez <juanestebanrp123@gmial.com>
2024-09-06 10:57:15 -0500

    firebase

---
+++ Prodont/src/Services/firebase-config.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/compat/app"
+// TODO: Add SDKs for Firebase products that you want to use
+
+export const app = firebase.initializeApp ({
+  apiKey: "AIzaSyCiiwim5gi5WdNvtUX-Px0GIGCHd9Rm_Ec",
+  authDomain: "prodont-bd5eb.firebaseapp.com",
+  projectId: "prodont-bd5eb",
+  storageBucket: "prodont-bd5eb.appspot.com",
+  messagingSenderId: "479409623791",
+  appId: "1:479409623791:web:8dabdcec0cf264c5b8011a"
+});
