26b2d27b839fe0e44c70cd72e9022a228150672f
Juan Esteban Ramirez <juanestebanrp123@gmial.com>
2024-09-05 21:13:01 -0500

    CardServiceHome check

--- Prodont/src/Components/CardServicesHome/CardServicesHome.jsx
+++ Prodont/src/Components/CardServicesHome/CardServicesHome.jsx
@@ -1,17 +1,14 @@
 export function CardServicesHome(props) {
-    const { image, title } = props;
-  
-    return (
-      <>
-      <section>
-        <div>
-          <img className="w-60 z-20" src={image} alt="image" />
-          <h3 className="text-zinc-900">{title}</h3>
-        </div>
-      </section>
+  const { image, title } = props;
 
-        
-      </>
-    );
-  }
-  
+  return (
+    <div className="flex p-4">
+      <div className="flex bg-gradient-to-r from-purple-200/50 via-blue-200/50 to-cyan-400/50 rounded shadow-md p-4 rounded-lg">
+        <div className="flex flex-col items-center w-64 h-56">
+          <img className="w-60 h-40 mb-4 rounded-lg object-cover" src={image} alt={title} />
+          <h3 className="text-violet-800 font-FuturaBook text-lg text-center">{title}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
--- Prodont/src/screens/Home/Home.jsx
+++ Prodont/src/screens/Home/Home.jsx
@@ -1,34 +1,40 @@
-import { NavigationBar,FeedCard,SecondTitle,ThirdTitle,CardServicesHome} from "../../Components/index"
+import { NavigationBar, FeedCard, SecondTitle, ThirdTitle, CardServicesHome } from "../../Components/index";
+
 export function Home() {
-  
   return (
     <>
+      <NavigationBar />
+      <FeedCard />
+      <SecondTitle SecondTitle="Servicios" />
 
-    <NavigationBar/>
-    <FeedCard/>
-    <SecondTitle SecondTitle="Servicios"/>
-    <CardServicesHome title="Ortodoncia"    
-                      image="/assets/ServiceCard/Ortodoncia.jpg" 
-    />
-    <CardServicesHome title="Diseño De Sonrisa"   
-                      image="/assets/ServiceCard/Diseño-Sonrisa.jpg"
-    />
-    <CardServicesHome title="Aclaramiento Dental" 
-                      image="/assets/ServiceCard/Aclaramiento-Dental.jpg" 
-    />
-    <CardServicesHome title="Protesis Dental"  
-                      image="/assets/ServiceCard/Protesis.jpg" 
-    />
-    <CardServicesHome title="Odontología Estetica" 
-                      image="/assets/ServiceCard/Odontologia-Estetica.jpg" 
-    />
-    <CardServicesHome title="Odontología General" 
-                      image="/assets/ServiceCard/Odontologia-General.jpg"
-    />
-    <ThirdTitle ThirdTitle="Articulos"/>
+      <div className="flex flex-wrap justify-center gap-6 p-4">
+        <CardServicesHome
+          title="Ortodoncia"
+          image="/assets/ServiceCard/Ortodoncia.jpg"
+        />
+        <CardServicesHome
+          title="Diseño De Sonrisa"
+          image="/assets/ServiceCard/Diseño-Sonrisa.jpg"
+        />
+        <CardServicesHome
+          title="Aclaramiento Dental"
+          image="/assets/ServiceCard/Aclaramiento-Dental.jpg"
+        />
+        <CardServicesHome
+          title="Protesis Dental"
+          image="/assets/ServiceCard/Protesis.jpg"
+        />
+        <CardServicesHome
+          title="Odontología Estetica"
+          image="/assets/ServiceCard/Odontologia-Estetica.jpg"
+        />
+        <CardServicesHome
+          title="Odontología General"
+          image="/assets/ServiceCard/Odontologia-General.jpg"
+        />
+      </div>
 
+      <ThirdTitle ThirdTitle="Artículos" />
     </>
-  )
+  );
 }
-
-
