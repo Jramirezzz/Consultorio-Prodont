d683751bf12ad6553787a1837347d68f16038bf2
Juan Esteban Ramirez <juanestebanrp123@gmial.com>
2024-09-05 22:49:49 -0500

    Navigation

--- Prodont/src/Components/NavBar/NavigationBar.jsx
+++ Prodont/src/Components/NavBar/NavigationBar.jsx
@@ -1,53 +1,46 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import {Logo} from "./Logo.jsx";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import { Link as RouterLink } from "react-router-dom";
+import { Logo } from "./Logo.jsx";
 
-export  function NavigationBar() {
+export function NavigationBar() {
   return (
-    <Navbar className="bg-gradient-to-r from-purple-200 via-blue-200 to-cyan-400 bg-opacity-10 backdrop-blur-sm rounded" 
-            flex maxWidth="full" 
-            position="sticky" 
-            justify="end" 
-            isBlurred="true">
+    <Navbar 
+      className="bg-gradient-to-r from-purple-200 via-blue-200 to-cyan-400 bg-opacity-10 backdrop-blur-sm rounded" 
+      maxWidth="full" 
+      position="sticky" 
+      justify="end" 
+      isBlurred={true}
+    >
       <NavbarBrand>
         <Logo />
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" 
-                     justify="center">
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link className="text-purple-700 font-bold" href="#">
-            Principal
-          </Link>
+          <RouterLink to="/" className="text-purple-700 font-bold">
+            Home
+          </RouterLink>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link className="text-purple-500" href="#" aria-current="page">
+        <NavbarItem>
+          <RouterLink to="/news" className="text-purple-500" aria-current="page">
             Noticias
-          </Link>
+          </RouterLink>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-purple-500" href="#">
+          <RouterLink to="/services" className="text-purple-500">
             Servicios
-          </Link>
+          </RouterLink>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} 
-                  color="primary" 
-                  href="#" 
-                  variant="flat">
-          <svg viewBox="0 0 20 20"   
-               xmlns="http://www.w3.org/2000/svg" 
-               fill="none"  
-               stroke="#2B0496"><g  
-               id="SVGRepo_bgCarrier"  
-               strokeWidth="0"></g><g  
-               id="SVGRepo_tracerCarrier"  
-               strokeLinecap="round"  
-               strokeLinejoin="round"></g><g  
-               id="SVGRepo_iconCarrier"> <path  
-               fill="#2B0496"  
-               fillRule="evenodd"  
-               d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"></path> </g></svg> 
+          <Button as="a" color="primary" href="#" variant="flat">
+            <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#2B0496">
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path fill="#2B0496" fillRule="evenodd" d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"></path>
+              </g>
+            </svg>
           </Button>
         </NavbarItem>
       </NavbarContent>
--- Prodont/src/screens/Home/Home.jsx
+++ Prodont/src/screens/Home/Home.jsx
@@ -34,6 +34,33 @@ export function Home() {
       </div>
 
       <ThirdTitle ThirdTitle="Artículos" />
+
+      <div className="flex flex-wrap justify-center gap-6 p-4">
+        <CardServicesHome
+          title="Ortodoncia"
+          image="/assets/ServiceCard/Ortodoncia.jpg"
+        />
+        <CardServicesHome
+          title="Diseño De Sonrisa"
+          image="/assets/ServiceCard/Diseño-Sonrisa.jpg"
+        />
+        <CardServicesHome
+          title="Aclaramiento Dental"
+          image="/assets/ServiceCard/Aclaramiento-Dental.jpg"
+        />
+        <CardServicesHome
+          title="Protesis Dental"
+          image="/assets/ServiceCard/Protesis.jpg"
+        />
+        <CardServicesHome
+          title="Odontología Estetica"
+          image="/assets/ServiceCard/Odontologia-Estetica.jpg"
+        />
+        <CardServicesHome
+          title="Odontología General"
+          image="/assets/ServiceCard/Odontologia-General.jpg"
+        />
+      </div>
     </>
   );
 }
